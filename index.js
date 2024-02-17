26967b4d97a57939a8a6a317fa7a1229909f7467
SURYAKANTSHARMA <suryakantsharma84@gmail.com>
2024-02-17 19:16:22 +0530

    complete project
    - install multer for upload file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer();
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +13,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'),
+     function (req, res) {
+  /**
+   * When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+ */
+const file = req.file
+if (!file) {
+  return res.status(400).json({ error: 'No file uploaded' });
+} 
 
+res.json({
+  name: file.originalname,
+  type: file.mimetype,
+  size: file.size
+});
+
+});
 
 
 const port = process.env.PORT || 3000;
